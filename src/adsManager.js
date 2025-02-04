8b0c446c56c76b07a94fb848c4f14ce5f59da487
Badr Eddine El Kalai <115181341+BadrKali@users.noreply.github.com>
2025-02-04 09:30:12 +0100

    Feat : Implemented ads Manager class for managing everything related to campaigns and reports

---
+++ src/adsManager.js
@@ -0,0 +1,28 @@
+const apiClient = require('./apiClient')
+require("dotenv").config();
+
+
+
+
+class AdsManager {
+    constructor(sdkConfig, refreshToken) {
+        this.apiClient = new apiClient({
+            clientId: sdkConfig.clientId,
+            clientSecret: sdkConfig.clientSecret,
+            redirectUri: sdkConfig.redirectUri,
+            refreshToken: refreshToken
+        });
+    }
+
+    async getAllCampaigns(adAccountId) {
+        return this.apiClient.get(`/v1/adaccounts/${adAccountId}/campaigns`);
+    }
+
+    async getSpecificCampaign(campaignId) {
+        return this.apiClient.get(`/v1/campaigns/${campaignId}`);
+    }
+
+    async deleteCampaign(campaignId) {
+        return this.apiClient.delete(`/v1/campaigns/${campaignId}`);
+    }
+}
